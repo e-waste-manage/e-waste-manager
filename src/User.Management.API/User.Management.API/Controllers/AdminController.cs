7ed363679ec88bfab9e702a43f31fb86239da542
Lenon Koh Herdyi <lenon.herdyi@ncs.com.sg>
2023-10-14 11:17:12 +0800

    Update Account Service

--- src/User.Management.API/User.Management.API/Controllers/AdminController.cs
+++ src/User.Management.API/User.Management.API/Controllers/AdminController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using User.Management.API.Models;
+using User.Management.API.Models.Authentication.SignUp;
 
 namespace User.Management.API.Controllers
 {
@@ -8,10 +13,32 @@ namespace User.Management.API.Controllers
     [ApiController]   
     public class AdminController : ControllerBase
     {
-        [HttpGet("employees")]
-        public IEnumerable<string> Get()
+        private readonly UserManager<ApplicationUser> _dbcontext;
+
+        public AdminController(UserManager<ApplicationUser> _context)
         {
-            return new List<string> { "Ahmed", "Ali", "Ahsan" };
+            _dbcontext = _context;
+        }
+
+
+        [HttpGet("GetUserDetailsByID")]
+        public async Task<IActionResult> GetUserDetailsByID(string UserID)
+        {
+            var userExist = await _dbcontext.FindByIdAsync(UserID);
+
+            if (userExist == null)
+            {
+                return NotFound();
+            }
+            var UserDetails = _dbcontext.Users.Select(t => new
+            {
+                t.Id, t.Name, t.Email,t.PhoneNumber, t.Address
+            }
+            ).Where(t => t.Id == UserID).FirstOrDefault();
+            return Ok(UserDetails);
+
         }
     }
 }
+
+
